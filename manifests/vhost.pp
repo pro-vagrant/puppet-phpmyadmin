a06979de3a1fcba49ee6ab628c634ec862dc5ff4
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-07-26 10:04:43 +0200

    Add phpmyadmin.lh to /etc/hosts

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -14,4 +14,9 @@ class phpmyadmin::vhost {
         ],
     }
 
+    file_line { 'add_phpmyadmin.lh_domain_in_hosts_file':
+        path => '/etc/hosts',
+        line => "127.0.0.1   phpmyadmin.lh"
+    }
+
 }
